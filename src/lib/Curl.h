bf5d4d1019730c97aec2e450dfc0ffd166e1f795
Paul D Handy <paul.d.handy@gmail.com>
2017-04-01 09:17:00 -0600

    made init_curl exportable

--- src/lib/Curl.h
+++ src/lib/Curl.h
@@ -13,7 +13,7 @@ typedef struct _Curl {
 	trit_t state[STATE_LENGTH];
 } Curl;
 
-void init_curl(Curl *ctx);
+EXPORT void init_curl(Curl *ctx);
 
 EXPORT void absorb(Curl *ctx, trit_t *const trits, int offset, int length);
 EXPORT void squeeze(Curl *ctx, trit_t *const trits, int offset, int length);
